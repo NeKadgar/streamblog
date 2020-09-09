485f0f6ed24ad407c1971dc99b5ac27f4f49a2c5
NeKadgar <37880360+NeKadgar@users.noreply.github.com>
2020-09-09 19:45:00 +0300

    update admin page

--- server/fishbot/admin.py
+++ server/fishbot/admin.py
@@ -3,6 +3,16 @@ from .models import Subscribtion, UserAction, TelegramToken
 
 # Register your models here.
 
-admin.site.register(Subscribtion)
-admin.site.register(UserAction)
 admin.site.register(TelegramToken)
+
+@admin.register(Subscribtion)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'end', 'active')
+
+
+@admin.register(UserAction)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'time', 'ip')
+    fields = ('user', 'time', 'ip')
+    list_filter = ('user',)
+    readonly_fields = ['time']

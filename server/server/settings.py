9317c2d34077403079c9ae86e04f372b381a6fdb
NeKadgar <37880360+NeKadgar@users.noreply.github.com>
2020-09-21 16:29:28 +0300

    fix static root

--- server/server/settings.py
+++ server/server/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 PROFILE_SESSION_ID = 'profile'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'

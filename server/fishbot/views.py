e3f1b8288c06fe510a6d2e11915768fcd0c4a987
NeKadgar <maximsheleh@gmail.com>
2020-12-02 15:27:42 +0300

    five minutes check

--- server/fishbot/views.py
+++ server/fishbot/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from .models import Subscribtion, UserAction, TelegramToken
 import datetime
 from django.utils import timezone
-
+import requests
 # Create your views here.
 
 def check_status(request, token):
@@ -19,6 +19,12 @@ def check_status(request, token):
         return JsonResponse(
             {"sub_status": False, "message": "Incorrect token", "sub_time": "..."})
 
+def send_telegram_message(bot_message):
+    bot_token = "865299523:AAEwrvzTwgvebQzABuTT-D9T3i_JDRyg_fY"
+    bot_chat_id = str(405089115)
+    api_url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + str(bot_message)
+    requests.get(api_url)
+
 def user_action(request, token):
     sub = Subscribtion.objects.filter(token=token).first()
     if sub.start < timezone.now() < sub.end:
@@ -34,7 +40,11 @@ def user_action(request, token):
     sub = Subscribtion.objects.filter(token=token, active=True).first()
     if sub:
         created_time = timezone.now() - datetime.timedelta(minutes=60)
+        five_minutes = timezone.now() - datetime.timedelta(minutes=5)
         actions = UserAction.objects.filter(user=sub.user).order_by('-pk')
+        if actions and actions[0].time < five_minutes:
+            message = actions.user.username + " check this user!!!"
+            send_telegram_message(message)
         if actions and actions[0].time > created_time:
             last_action = actions[0]
             if last_action.ip == ip:
